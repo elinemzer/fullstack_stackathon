ecd1a5518268bc5a1fd8d16f79b0c96cebe0c066
Eli Nemzer <elinemzer7@gmail.com>
2017-06-17 12:01:55 -0400

    map filters properly

--- browser/components/Togglebar.js
+++ browser/components/Togglebar.js
@@ -7,12 +7,12 @@ class Togglebar extends Component {
       return (
 
         <div className="btn-group" role="group" aria-label="...">
-          <button type="button" className="btn btn-default">1950</button>
-          <button type="button" className="btn btn-default">1970</button>
-          <button type="button" className="btn btn-default">1990</button>
-          <button type="button" className="btn btn-default">2010</button>
-          <button type="button" className="btn btn-default">2030</button>
-          <button type="button" className="btn btn-default">2050</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle1950submit}>1950</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle1970submit}>1970</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle1990submit}>1990</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle2010submit}>2010</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle2030submit}>2030</button>
+          <button type="button" className="btn btn-default" onClick={this.props.handle2050submit}>2050</button>
         </div>
   )
  }
--- browser/containers/MapContainer.js
+++ browser/containers/MapContainer.js
@@ -10,20 +10,13 @@ export default class MapContainer extends Component {
       markers: []
     }
     this.onCityClick = this.onCityClick.bind(this);
+    this.handle1950submit = this.handle1950submit.bind(this);
+    this.handle1970submit = this.handle1970submit.bind(this);
+    this.handle1990submit = this.handle1990submit.bind(this);
+    this.handle2010submit = this.handle2010submit.bind(this);
+    this.handle2030submit = this.handle2030submit.bind(this);
+    this.handle2050submit = this.handle2050submit.bind(this);
   }
-
-componentDidMount(){
-  const markerData = data;
-  const allMarkers = markerData.map(markerObject => {
-    const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
-    return {
-      position: latlng
-    }
-  })
-  this.setState({
-    markers: allMarkers
-  })
-}
 
 onCityClick(clickedCity) {
   this.setState({
@@ -49,7 +42,124 @@ onCityClick(clickedCity) {
   })
 }
 
+handle1950submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.fiftypop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+handle1970submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.seventypop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+handle1990submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.ninetypop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+handle2010submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.tenpop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+handle2030submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.thirtypop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+
+handle2050submit(){
+  const markerData = data
+  const filteredMarkers = markerData.filter(markerObject => {
+    if (markerObject.twentyfiftypop[0].population){
+      return markerObject
+    }
+  })
+  const selectedMarkers = filteredMarkers.map(markerObject => {
+      const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+      console.log(latlng)
+      return {
+        position: latlng
+      }
+    })
+  this.setState({
+    markers: selectedMarkers
+  })
+}
+
+
   render(){
+    console.log('markerstate: ', this.state.markers)
     return (
       <div className="main-container">
         <Map
@@ -60,7 +170,37 @@ onCityClick(clickedCity) {
           markers={this.state.markers}
           onCityClick={this.onCityClick}
         />
+        <Togglebar
+          handle1950submit={this.handle1950submit}
+          handle1970submit={this.handle1970submit}
+          handle1990submit={this.handle1990submit}
+          handle2010submit={this.handle2010submit}
+          handle2030submit={this.handle2030submit}
+          handle2050submit={this.handle2050submit}
+        />
       </div>
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+// componentDidMount(){
+//   const markerData = data;
+//   const allMarkers = markerData.map(markerObject => {
+//     const latlng = {lat: Number(markerObject.latitude), lng: Number(markerObject.longitude)}
+//     return {
+//       position: latlng
+//     }
+//   })
+//   this.setState({
+//     markers: allMarkers
+//   })
+// }
